4444c24b7f5baac292fdae2e0661d6a377494ddf
ekucera1 <ekucera@uchicago.edu>
2024-10-22 17:35:03 +0000

    lots of changes

--- app.rb
+++ app.rb
@@ -7,3 +7,52 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+
+get("/rock") do
+  moves = ["rock", "paper", "scissors"]
+
+  @comp_move = moves.sample
+
+  if @comp_move == "rock"
+    @outcome = "tied"
+  elsif @comp_move == "paper"
+    @outcome = "lost"
+  else
+    @outcome = "won"
+  end
+  
+  erb(:zebra)
+end
+
+get("/paper") do
+  moves = ["rock", "paper", "scissors"]
+
+  @comp_move = moves.sample
+
+  if @comp_move == "paper"
+    @outcome = "tied"
+  elsif @comp_move == "scissors"
+    @outcome = "lost"
+  else
+    @outcome = "won"
+  end
+  
+  erb(:giraffe)
+end
+
+get("/scissors") do
+  moves = ["rock", "paper", "scissors"]
+
+  @comp_move = moves.sample
+
+  if @comp_move == "scissors"
+    @outcome = "tied"
+  elsif @comp_move == "rock"
+    @outcome = "lost"
+  else
+    @outcome = "won"
+  end
+  
+  erb(:lion)
+end
